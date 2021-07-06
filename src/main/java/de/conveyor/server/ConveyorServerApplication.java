1b9fb19e3330c3374d89ca98b1eabef2bdcc7733
Jonas <j.hasenauer98@gmail.com>
2021-07-06 09:29:29 +0200

    init springboot

---
+++ src/main/java/de/conveyor/server/ConveyorServerApplication.java
@@ -0,0 +1,13 @@
+package de.conveyor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConveyorServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/conveyor/server/ConveyorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.conveyor.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
